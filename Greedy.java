a9aeda4b15d85b1dd4f7a8113b981204516d0185
michal <mp.szmigiel@gmail.com>
2013-04-29 12:22:00 +0300

    Update Greedy.java

--- Greedy.java
+++ Greedy.java
@@ -1,9 +1,5 @@
-package JavaTutorials;
-
-import java.util.Scanner;
-
 public class Greedy {
-  final static int n = 3;
+	final static int n = 3;
 	public static void greedy(double m, double w[], double c[], double x[]){
 		int i;
 		double z=m;
@@ -18,38 +14,8 @@ public class Greedy {
 		}
 	}
 	public static void main(String[] args) {
-		double a1 = 0.0, b1 = 0.0, c1 = 0.0;
-		double a2 = 0.0, b2 = 0.0, c2 = 0.0;
-		double a3 = 0.0, b3 = 0.0, c3 = 0.0;
-		double w[]={a1,b1,c1}, c[]={a2,b2,c2}, x[]={a3,b3,c3};
-		double m;
-		Scanner gree = new Scanner(System.in);
-		System.out.print("Podaj M : ");
-		m=gree.nextDouble();
-		System.out.println("Dla w[i]");
-		System.out.print("Podaj pierwsza wartosc : ");
-		a1=gree.nextDouble();
-		System.out.print("Podaj druga wartosc : ");
-		b1=gree.nextDouble();
-		System.out.print("Podaj trzecia wartosc : ");
-		c1=gree.nextDouble();
-		System.out.println(" ");
-		System.out.println("Dla c[i]");
-		System.out.print("Podaj pierwsza wartosc : ");
-		a2=gree.nextDouble();
-		System.out.print("Podaj druga wartosc : ");
-		b2=gree.nextDouble();
-		System.out.print("Podaj trzecia wartosc : ");
-		c2=gree.nextDouble();
-		System.out.println(" ");
-		System.out.println("Dla x[i]");
-		System.out.print("Podaj pierwsza wartosc : ");
-		a3=gree.nextDouble();
-		System.out.print("Podaj druga wartosc : ");
-		b3=gree.nextDouble();
-		System.out.print("Podaj trzecia wartosc : ");
-		c3=gree.nextDouble();
-		System.out.println(" ");
+		double w[]={10,12,16}, c[]={60,70,80}, x[]={0,0,0};
+		double m = 20;
 		greedy(m,w,c,x);
 		double p=0;
 		System.out.println("i     w[i]     c[i]     x[i]");
